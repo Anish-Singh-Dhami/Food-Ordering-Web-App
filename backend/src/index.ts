d1824ab80b936f18654cc2c4d2c85c9340d9a941
Anish Singh Dhami <dhamianishsingh@gmail.com>
2025-02-09 21:57:13 +0530

    Connected to MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 
 const app = express();
 const PORT = 8000;
+
+const connectToDB = async() => { 
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING!);
+        console.log("Connected to DB successfully.")
+    } catch (error) {
+        console.error("Failed to connect to the DB ", error);
+    }
+};
+
+connectToDB();
 
 app.use(express.json());
 app.use(cors());
