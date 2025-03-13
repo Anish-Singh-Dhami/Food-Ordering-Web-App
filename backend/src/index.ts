439ca898c3b87f7cac9749e583c6747a1ec1da3d
Anish Singh Dhami <dhamianishsingh@gmail.com>
2025-03-13 17:01:44 +0530

    Building backend code by generating JS code using tsc typecript compiler.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,18 +4,17 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
 
-
 const app = express();
 const port: string = process.env.PORT!;
-const connectionString : string = process.env.MONGODB_CONNECTION_STRING!;
+const connectionString: string = process.env.MONGODB_CONNECTION_STRING!;
 
-const connectToDB = async() => { 
-    try {
-        await mongoose.connect(connectionString);
-        console.log("Connected to DB successfully.")
-    } catch (error) {
-        console.error("Failed to connect to the DB ", error);
-    }
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(connectionString);
+    console.log("Connected to DB successfully.");
+  } catch (error) {
+    console.error("Failed to connect to the DB ", error);
+  }
 };
 
 connectToDB();
@@ -26,15 +25,18 @@ app.use(cors());
 /**
  * Forward the request starting with /api/my/user to myUserRoutes.
  * Containing controllers to handle business logic based on the rest of the request.
- */ 
+ */
 app.use("/api/my/user", MyUserRoute.router);
 
-app.get("/", async (req: Request, res: Response) => {
-    res.json({
-        message : "Hi"
-    });
-})
+/**
+ * Health request to check our backend in working / running while in production.
+ */
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({
+    message: "Health is ok!",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server started and listening  at http://localhost:${port}`);
-})
+  console.log(`Server started and listening  at http://localhost:${port}`);
+});
